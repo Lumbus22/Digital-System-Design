56134f6c7868c697b82d2987fa88a75e8a244643
Columbus Enslen <117124101+Lumbus22@users.noreply.github.com>
2026-03-13 00:12:58 -0400

    Update Extend.v
    updated indentation

--- final_project/RISC-V32I-CPU/Extend.v
+++ final_project/RISC-V32I-CPU/Extend.v
@@ -10,9 +10,9 @@ always @ (*)
 	begin
 		case(ImmSrc)
 			3'b000: ImmExt = {{20{instruction[31]}}, instruction[31:20]}; // I-Type immidiate extension
-         3'b001: ImmExt = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};// S -type immidiate extension
-         3'b010: ImmExt = {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};// B - type immidiate instruction
-         3'b011: ImmExt = {{12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], 1'b0};// j - Type immidiate instruction
+	        3'b001: ImmExt = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};// S -type immidiate extension
+         	3'b010: ImmExt = {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};// B - type immidiate instruction
+         	3'b011: ImmExt = {{12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], 1'b0};// j - Type immidiate instruction
 			3'b100: ImmExt = {instruction[31:12], 12'b0}; // U - Type Instruction
 		endcase
 	end
